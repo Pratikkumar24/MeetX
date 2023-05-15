66ac84d1a8d88e92834a89485bfaa888a8277f98
Pratkkumar24 <pratikkumar200024@gmail.com>
2023-05-15 22:31:38 +0530

    UI training

--- app/src/main/java/com/Executioner/meetx/MainActivity.java
+++ app/src/main/java/com/Executioner/meetx/MainActivity.java
@@ -10,7 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //App got loaded with the layouts
 
+
+        //todo - do the code
 
     }
 }
