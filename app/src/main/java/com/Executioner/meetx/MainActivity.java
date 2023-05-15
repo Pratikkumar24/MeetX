dd6d88382216788db48eaef8db28f0b16986dcf9
Pratkkumar24 <pratikkumar200024@gmail.com>
2023-05-15 22:50:33 +0530

    Removing unecessary comments

--- app/src/main/java/com/Executioner/meetx/MainActivity.java
+++ app/src/main/java/com/Executioner/meetx/MainActivity.java
@@ -10,10 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //App got loaded with the layouts
 
 
-        //todo - do the code
 
     }
 }
