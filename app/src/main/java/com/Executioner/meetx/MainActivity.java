46cc41258de7854b259eafaa5a48d5398b622665
neha702 <natzdaz702@gmail.com>
2023-05-16 21:13:01 +0530

    First page on making

--- app/src/main/java/com/Executioner/meetx/MainActivity.java
+++ app/src/main/java/com/Executioner/meetx/MainActivity.java
@@ -2,16 +2,33 @@ package com.Executioner.meetx;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+Button next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        next=findViewById(R.id.btn_next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                test();
+            }
+        });
 
+    }
+    public void test(){
+        Context context = getApplicationContext();
+        CharSequence text = "Welcome to App!";
+        int duration = Toast.LENGTH_SHORT;
 
-
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
