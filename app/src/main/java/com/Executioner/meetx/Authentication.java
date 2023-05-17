1f96b108915b12d7a668f7d6d61de466e7464954
Pratkkumar24 <pratikkumar200024@gmail.com>
2023-05-17 22:29:28 +0530

    Added signup page

--- app/src/main/java/com/Executioner/meetx/Authentication.java
+++ app/src/main/java/com/Executioner/meetx/Authentication.java
@@ -2,6 +2,7 @@ package com.Executioner.meetx;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,15 +10,26 @@ import android.widget.Button;
 
 public class Authentication extends AppCompatActivity {
 Button login_btn;
+Button signup_btn;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
-        login_btn=findViewById(R.id.Login);
+        login_btn=findViewById(R.id.Login_page);
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(getApplicationContext(),Login_page.class);
+                startActivity(intent);
+            }
+        });
+
+        signup_btn = findViewById(R.id.Signup_page);
+        signup_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Signup_page.class);
                 startActivity(intent);
             }
         });
