39cc5a7e18963959e352c46fa789cbaedadeaac6
neha702 <natzdaz702@gmail.com>
2023-05-25 21:21:21 +0530

    Adding splash screen

--- app/src/main/java/com/Executioner/meetx/MainActivity.java
+++ app/src/main/java/com/Executioner/meetx/MainActivity.java
@@ -4,34 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.Executioner.meetx.authentication.Authentication;
+import com.Executioner.meetx.authentication.Login_page;
 
 public class MainActivity extends AppCompatActivity {
-ImageView next;
     //Context context = getApplicationContext();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        next=findViewById(R.id.btn_next);
-        next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(getApplicationContext(), Authentication.class);
-                startActivity(intent);
-            }
-        });
+       new Handler().postDelayed(new Runnable() {
+           @Override
+           public void run() {
+               Intent intent=new Intent(getApplicationContext(), Authentication.class);
+               startActivity(intent);
+               finish();
+           }
+       },4000);
 
-    }
-    public void test(){
-        CharSequence text = "Welcome to App!";
-        int duration = Toast.LENGTH_SHORT;
-
-        Toast toast = Toast.makeText(getApplicationContext(), text, duration);
-        toast.show();
     }
 }
