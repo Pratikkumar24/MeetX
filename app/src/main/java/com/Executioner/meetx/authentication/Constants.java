5e4497fed011f992cf54f87d7638c3492acf64e0
Pratikkumar200024 <pratikkumar200024@gmail.com>
2023-06-03 15:34:27 +0530

    Change in database(addition of phoneno)

--- app/src/main/java/com/Executioner/meetx/authentication/Constants.java
+++ app/src/main/java/com/Executioner/meetx/authentication/Constants.java
@@ -8,11 +8,12 @@ public class Constants {
     public static final Integer USERNAME_MAX_LENGTH = 15;
     public static final String DEFAULT_USERNAME="meetx";
     public static final String DEFAULT_PASSWORD="Abc@16";
+    public static final String DEFAULT_PHONE_NUMBER = "1234567890";
     public static final String DEFAULT_EMAIL="abc@gmail.com";
     public static final String EMAIL="email";
     public static final String PASSWORD="password";
     public static final String NULL="null";
     public static final String USERS="users";
-    public static final int HIGHBOUND=9999;
-    public static final int LOWBOUND=1000;
+    public static final int HIGH_BOUND=9999;
+    public static final int LOW_BOUND=1000;
 }
